bae2e035ddfa0410df76b7f0f4754cc9e51e785e
kennethj3 <349320036@gapps.yrdsb.ca>
2022-12-19 17:23:24 -0500

    Added zombie

--- MyWorld.java
+++ MyWorld.java
@@ -16,10 +16,12 @@ public class MyWorld extends World
     public MyWorld()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1);
+        super(700, 500, 1);
         
         Soldier soldier = new Soldier();
         addObject(soldier, 40, 200);
+        
+        Zombie zombie = new Zombie();
         
         
     }
---
+++ Zombie.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Zombie here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Zombie extends Actor
+{
+    /**
+     * Act - do whatever the Zombie wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    int speed = 1;
+    public void act()
+    {
+        int x = getX() - speed;
+        int y = getY();// Add your action code here.
+    }
+}
